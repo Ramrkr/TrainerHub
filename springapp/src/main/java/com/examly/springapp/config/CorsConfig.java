6417bacfe1d44a5d99dd7089b563d2baaaf51516
Ram <ramrkr2016@gmail.com>
2026-01-15 14:01:24 +0530

    added env for angular

--- springapp/src/main/java/com/examly/springapp/config/CorsConfig.java
+++ springapp/src/main/java/com/examly/springapp/config/CorsConfig.java
@@ -12,7 +12,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
 public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-    .allowedOriginPatterns("*")
+    //.allowedOriginPatterns("*")
+    .allowedOrigins("https://mytrainerhub.netlify.app","http://localhost:8081")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true);
